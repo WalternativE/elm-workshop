23fbde9c7199e2c309aef0659759c57404576837
WalternativE <gregor.beyerle@gmail.com>
2017-12-06 12:02:16 +0100

    commiting workshop progress

--- src/Main.elm
+++ src/Main.elm
@@ -1,38 +1,86 @@
 module Main exposing (..)
 
-import Html exposing (div, h1, text)
-import Html.Attributes as Attributes exposing (class)
+import Html exposing (Html, div, h1, text, label, input, i)
+import Html.Attributes as Attributes exposing (class, type_, placeholder, value)
+import Html.Events exposing (onClick, onInput)
+import String
 
 
 ---- MODEL ----
 
+type alias Model = {
+        friendsList : List String,
+        inputValue : String
+    }
 
+init : (Model, Cmd msg)
 init =
-    ( "", Cmd.none )
-
+    ( { friendsList = ["Bruce Bannner", "Tony Stark"], inputValue = "" }, Cmd.none )
 
 
 ---- UPDATE ----
+type Msg
+    = RemoveFriend String
+    | UpdateInputValue String
+    | AddFriend
 
-
+update : Msg -> Model -> (Model, Cmd msg)
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        RemoveFriend name ->
+            let
+                fl =
+                    model.friendsList
+                    |> List.filter ((/=) name)
+            in
+                ({ model | friendsList = fl }, Cmd.none)
 
+        UpdateInputValue inp ->
+            ({ model | inputValue = inp }, Cmd.none)
 
+        AddFriend ->
+            if String.isEmpty model.inputValue then
+                (model, Cmd.none)
+            else
+                let
+                    n = model.inputValue
+                in
+                    ({ model | friendsList = n :: model.friendsList, inputValue = "" }, Cmd.none)
 
 ---- VIEW ----
 
+cardView : String -> Html Msg
+cardView name =
+    div [ class "card" ] [
+        div [ class "content" ] [
+            i [ class "right floated remove red icon link"
+                , onClick <| RemoveFriend name ] []
+            , div [ class "description" ] [ text name ]
+        ]
+    ]
 
-view model =
-    div [ class "ui main text container" ]
-        [ h1 [ class "ui header" ]
-            [ text "Your Elm App is Working" ]
+view {inputValue, friendsList } =
+    div [ class "ui main text container" ] [
+            h1 [ class "ui header" ] [ text "Friends List" ]
+            , div [ class "ui segment"] [
+                div [ class "ui form" ] [
+                    div [ class "field" ] [
+                        label [] [ text "Name" ]
+                        , input [ type_ "text"
+                                  , placeholder "Name"
+                                  , onInput UpdateInputValue
+                                  , value inputValue ] []
+                    ]
+                    , div [ class "ui button", onClick AddFriend ] [ text "Add a friend" ]
+                ]
+            ]
+            , div [ class "ui two column cards" ] 
+                 (List.map cardView friendsList)
         ]
 
 
 
 ---- PROGRAM ----
-
 
 main =
     Html.program
